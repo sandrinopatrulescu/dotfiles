f76e5dc7a303a5f562f75ba520986a43cbf92673
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2022-07-15 18:17:00 +0300

    [windows] TODO: deal with duplicate values in PATH

--- windows/env.cmd
+++ windows/env.cmd
@@ -4,14 +4,11 @@
 set DOTSWB="%DOTSW%\bin"
 set LOGDIR=%USERPROFILE%\logs
 
-
+:: TODO deal with duplicate path values due to calls to "src"
 SET PATH=%DOTSWB%;%PATH% & :: https://www3.ntu.edu.sg/home/ehchua/programming/howto/Environment_Variables.html
 
 
 :: system dependent
-:: TODO: why setting UU? remove UU if not found used with grep
-:: TODO: automatically download UnxUtils on install
-set UU="E:\Programs\UnxUtils\usr\local\wbin"
 set UU="%DOTSW%\external-tools\UnxUtils\usr\local\wbin"
 
 SET PATH=%UU%;%PATH%
