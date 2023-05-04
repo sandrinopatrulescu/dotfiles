aef6210d5e990a44f35e55c71dbd51a0cb7d85cb
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2023-05-04 23:25:15 +0300

    finish check-dots-repo.sh

--- bin/check-dots-repo.sh
+++ bin/check-dots-repo.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+#set -x
 
-sleep 1
-# set the path to your git repository
-repo_path="/mnt/e/dotfiles"
+. ~/.env
+. "$DOTS"/linux/functions.sh
 
-# check if the repository has changed
-cd "$repo_path" || { for _ in {1..98}; do notify-send "dotfiles UPDATE" "COULD NOT FIND REPO PATH"; done; exit; }
-#if true; then
-if [[ $(git fetch --dry-run 2>&1 | grep -c -E '^\s*[a-zA-Z]') -gt 0 ]]; then
-    # if there are changes, display a notification
-    # repeat command to update the notification
-    for _ in {1..98}; do notify-send "dotfiles UPDATE" "Git repository has changed\t\t\t"; done
+
+internetConnectionTrials=20
+while ! is_internet_working && [ $internetConnectionTrials -gt 0 ]; do
+    ((internetConnectionTrials--))
+    repeat 5 notify-send "dotfiles UPDATE" "Internet not working\n${internetConnectionTrials} trials left"
+    sleep 6.5
+done
+
+if ! is_internet_working; then
+    repeat 5 notify-send "dotfiles UPDATE" "Internet not working... EXITING\nPlease connect to internet and run $(basename $0)"
+else
+    cd "$DOTS" || { repeat 98 notify-send "dotfiles UPDATE" "COULD NOT FIND REPO PATH"; exit; }
+    # check if the repository has changed
+    #if true; then
+    if [[ $(git fetch --dry-run 2>&1 | grep -c -E '^\s*[a-zA-Z]') -gt 0 ]]; then
+        # if there are changes, display a notification
+        # repeat command to update the notification
+        repeat 98 notify-send "dotfiles UPDATE" "Git repository has changed\t\t\t"
+    else
+        repeat 3 notify-send "dotfiles UPDATE" "No changes\t\t\t"
+    fi
 fi
