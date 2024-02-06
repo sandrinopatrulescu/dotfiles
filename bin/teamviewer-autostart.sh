f53b9f9dba7b512e810b5aa492a02a33f3a748c8
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2024-02-06 19:21:38 +0200

    wmctrl refactor: dynamic last workspace + pare()

--- bin/teamviewer-autostart.sh
+++ bin/teamviewer-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 teamviewer &
 sleep 2
-wmctrl -r TeamViewer -t 3
+wmctrl -r TeamViewer -t $(($(xfconf-query -c xfwm4 -p /general/workspace_count) - 1))
--- bin/transmission-autostart.sh
+++ bin/transmission-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 transmission-gtk &
 sleep 1
-wmctrl -r Transmission -t 3
+wmctrl -r Transmission -t $(($(xfconf-query -c xfwm4 -p /general/workspace_count) - 1))
