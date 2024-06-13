77a65651e08fe028b8268f5f2ffa9cf55da3eb47
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2024-06-14 00:11:39 +0300

    add argument parsing in yt-playlist-to-csv.sh
    Took 33 minutes

--- bin/yt-playlist-to-csv.sh
+++ bin/yt-playlist-to-csv.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+PLAYLIST_ID="PLqRTNdk3LL2hwXxYAW_-KY5kHH4V0U_EL"
+OUTPUT_FILE="" # default is PLAYLIST_NAME.csv (where PLAYLIST_NAME will be replaced from the response)
+COMPACT=""
+
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -pi|--playlist-id)
+            PLAYLIST_ID="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -p|--playlist)
+            PLAYLIST_ID="$(echo "$2" | /usr/bin/sed -n 's/.*list=\(.*\)/\1/p')"
+            shift # past argument
+            shift # past value
+            ;;
+        -o|--output)
+            OUTPUT_FILE="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        --compact)
+            COMPACT="--compat-options no-youtube-unavailable-videos"
+            shift # past argument
+            ;;
+        -*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 
 function join_by { # https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-a-bash-array-into-a-delimited-string
     local d=${1-} f=${2-}
@@ -9,13 +48,9 @@ function join_by { # https://stackoverflow.com/questions/1527049/how-can-i-join-
     fi
 }
 
-playlist_id="${1:-PLqRTNdk3LL2hwXxYAW_-KY5kHH4V0U_EL}"
-output_file="$2"
-
-playlist_url="https://www.youtube.com/playlist?list=$playlist_id"
-
-[ -n "$COMPACT" ] && extra_args="--compat-options no-youtube-unavailable-videos"
-command="yt-dlp $extra_args --flat-playlist -j $playlist_url "
+# playlist entry: https://pastebin.com/dry0Tz1G
+playlist_url="https://www.youtube.com/playlist?list=$PLAYLIST_ID"
+command="yt-dlp $COMPACT --flat-playlist -j $playlist_url "
 
 echo "running: $command"
 
@@ -27,7 +62,7 @@ playlistName=""
 while read -r json; do
     if [ -z "$playlistName" ]; then
         playlistName=$(jq -r .playlist <<< "$json")
-        join_by $delimiter "index" "${fields[@]}" | tee -a "${output_file:-$playlistName.csv}"
+        join_by $delimiter "index" "${fields[@]}" | tee -a "${OUTPUT_FILE:-$playlistName.csv}"
     fi
     line=$(jq -r .playlist_index <<< "$json")
 
@@ -36,5 +71,5 @@ while read -r json; do
         line="$line$delimiter$column"
     done
 
-    echo "$line" | tee -a "${output_file:-$playlistName.csv}"
+    echo "$line" | tee -a "${OUTPUT_FILE:-$playlistName.csv}"
 done <<< "$($command)"
