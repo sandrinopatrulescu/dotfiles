90818f73778b595037d6af155f08ad603df4c1e5
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2024-10-08 20:46:29 +0300

    minecraft services
    Took 29 minutes

---
+++ bin/minecraft-server-stop-on-gui.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+serviceName="minecraft-server.service"
+
+if systemctl is-active "${serviceName}"; then
+        sessionIds=$(loginctl --no-legend list-sessions | awk  '$2 = !/root|kamui/ {print $1}')
+        for sessionId in $sessionIds; do
+            session=$(loginctl show-session "$sessionId")
+            if echo "$session" | grep -qE '^Desktop='; then
+                echo "Desktop session found. Stopping ${serviceName}"
+                systemctl stop "${serviceName}"
+                exit 0
+            fi
+        done
+        echo "No desktop session found"
+    else
+        echo "${serviceName} is not active"
+fi
