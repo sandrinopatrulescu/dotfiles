d7d434dc0474f21614d4255eac4d85188a1a52e1
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2026-03-22 07:46:14 +0200

    yt-dlp - improve updating

--- bin/yt-dlp-update2.sh
+++ bin/yt-dlp-update2.sh
@@ -14,5 +14,6 @@ script_name="$(basename "${BASH_SOURCE[0]}")"
 
     echo
     echo ">>> pip install yt-dlp -U"
-    ~/.local/bin/pip install -U yt-dlp
+    # ~/.local/bin/pip install -U yt-dlp
+    ~/.local/bin/pip install -U "yt-dlp[default]" # Challenge solver lib script version 0.3.2 is not supported -> https://github.com/yt-dlp/yt-dlp/issues/15887
 } >> "$LOGS/${script_name}_${timestamp}.log" 2>&1
